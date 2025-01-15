2ccdc318f78451d447f0a5bca811918afcd783a1
mmagc <matheusgnann@gmail.com>
2025-01-15 13:14:26 -0300

    first commit

---
+++ src/scripts/engine.js
@@ -0,0 +1,35 @@
+const emojis = [ "🏎", "🏎", "🛸", "🛸", "🦝", "🦝", "🐱‍💻", "🐱‍💻", "🎮", "🎮", "🐱‍👤", "🐱‍👤", "⚡", "⚡", "🦅", "🦅" ];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for(let i = 0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick; 
+    document.querySelector(".game").appendChild(box);
+}
+
+function handleClick() {
+    if(openCards.length < 2) {
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2) {
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch() {
+    if (openCards[0].innerHTML !== openCards[1].innerHTML) {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    } else {
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    }
+
+    openCards = [];
+}
